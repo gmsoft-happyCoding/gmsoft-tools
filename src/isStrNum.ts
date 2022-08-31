1e67a5c6d27651f2a802da5f0ef5a0ed433bcd0e
dou <35251293+bdDandelion@users.noreply.github.com>
2022-08-31 14:36:35 +0800

    修复数字判定正则

--- src/isStrNum.ts
+++ src/isStrNum.ts
@@ -11,7 +11,7 @@
  * @returns {boolean}
  */
 const isStrNum = (testStr: string) => {
-  const reg = /^-?([1-9]\d*\.\d*|0\.\d*[1-9]\d*|0?\.0+|0)$/;
+  const reg = /^(\-|\+)?\d+(\.\d+)?$/;
   if (reg.test(testStr)) {
     return true;
   }
